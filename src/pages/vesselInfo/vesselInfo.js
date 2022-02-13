0623ce3aa313878328aac0f83477fd12feabf2f7
unknown <jrs341@me.com>
2022-02-13 16:05:07 -0600

    added inputs to vessel info and submit is working

--- src/pages/vesselInfo/vesselInfo.js
+++ src/pages/vesselInfo/vesselInfo.js
@@ -11,11 +11,12 @@ export function addVessel(event){
         else {
           data[elem[i].name] = elem[i].value; 
         }
-        addDoc(vesselCollection, data)
+        console.log('data', data);
+        /*addDoc(vesselCollection, data)
           .then(docRef => {
           //console.log("Document written with ID: ", docRef.id);
           localStorage.setItem('vesselRefId', docRef.id);
-          /*addDoc(emailsCollection, {
+          addDoc(emailsCollection, {
             to: data.email,
             message: {
               subject: 'Austin Mobile Marine Mechanic Service Request',
@@ -23,7 +24,7 @@ export function addVessel(event){
               
               Thank you for trusting Austin Mobile Marine Mechanic to service your vessel.  Please allow up to three days for scheduling. If you do not receive an option for scheduling within that timeframe, please feel free to call or send a text message to 512 200 3433.\n\nSincerely,\n\nJason Sinn\nAustin Mobile Marine Mechanic\n512 200 3433`,
             }
-          }).then(() => console.log('Queued email for delivery!'));*/
-      });
+          }).then(() => console.log('Queued email for delivery!'));
+      });*/
       console.log('storage', localStorage.getItem('vesselRefId'));
     };
--- src/pages/vesselInfo/vesselInfo.jsx
+++ src/pages/vesselInfo/vesselInfo.jsx
@@ -19,9 +19,23 @@ export class VesselInfo extends React.Component {
           <h1>Vessel Information</h1>
           <label for="reg">Registration Number:</label><br/>
           <input type="text" id="reg" name="reg" placeholder="TX-1234-AB"/><br/>
+          <br/>
 
+          <input type="radio" id="outboard" name="outboard"/>
+          <label for="outboard">Outboard</label><br/>
+
+          <input type="radio" id="sternDrive" name="sternDrive"/>
+          <label for="sternDrive">Sterndrive</label><br/>
+
+          <input type="radio" id="vDrive" name="vDrive"/>
+          <label for="vDrive">V-Drive</label><br/>
+
+          <input type="radio" id="jski" name="jski"/>
+          <label for="jski">Jet Ski</label><br/>
+          <br/>
+          
           <label for="vyear">Year</label><br/>
-          <input type="text" id="vyear" name="vyear"/><br/>
+          <input type="number" id="vyear" name="vyear"/><br/>
 
           <label for="vmake">Make</label><br/>
           <input type="text" id="vmake" name="vmake"/><br/>
@@ -30,11 +44,18 @@ export class VesselInfo extends React.Component {
           <input type="text" id="vmodel" name="vmodel"/><br/>
     
           <h1>Engine Information</h1>
+
+          <label for="eman">Engine Manufacturer:</label><br/>
+          <input type="text" id="eman" name="eman"/><br/>
+
+          <label for="ehp">Engine Horsepower:</label><br/>
+          <input type="text" id="ehp" name="ehp"/><br/>
+
           <label for="esn">Engine Serial Number:</label><br/>
           <input type="text" id="esn" name="esn"/><br/>
 
           <label for="eyear">Year</label><br/>
-          <input type="text" id="eyear" name="eyear"/><br/>
+          <input type="number" id="eyear" name="eyear"/><br/>
 
           <label for="emake">Make</label><br/>
           <input type="text" id="emake" name="emake"/><br/>
@@ -53,7 +74,7 @@ export class VesselInfo extends React.Component {
           <input type="text" id="sstate" name="sstate"/><br/>
     
           <label for="szip">Postal Code:</label><br/>
-          <input type="text" id="szip" name="szip"/><br/>
+          <input type="number" id="szip" name="szip"/><br/>
     
           <label for="specInstr">Special Instructions:</label><br/>
           <input type="text" id="specInstr" name="specInstr" placeholder="Please list any gate codes necessary to access the vessel, location of the keys, etc... "/><br/>
