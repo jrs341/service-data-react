e1108af4abc5a470adc0bc00ed4190b4ea5c71df
unknown <jrs341@me.com>
2022-02-13 14:05:02 -0600

    writing document ref to local storage

--- src/pages/customerInfo/customerInfo.js
+++ src/pages/customerInfo/customerInfo.js
@@ -6,15 +6,16 @@ export function addCustomer(event){
     let data = {};
     var elem = document.getElementById(event.target.id).elements;
       for(var i = 0; i < elem.length; i++)
-        if(elem[i].name === "" || elem[i].value === "Submit")
-          {console.log(elem[i].name)}
+        if(elem[i].name === "" || elem[i].value === "Submit"){}
+          //{console.log(elem[i].name)}
         else {
           data[elem[i].name] = elem[i].value; 
         }
         addDoc(customerCollection, data)
           .then(docRef => {
           //console.log("Document written with ID: ", docRef.id);
-          addDoc(emailsCollection, {
+          localStorage.setItem('ownerRefId', docRef.id);
+          /*addDoc(emailsCollection, {
             to: data.email,
             message: {
               subject: 'Austin Mobile Marine Mechanic Service Request',
@@ -22,6 +23,7 @@ export function addCustomer(event){
               
               Thank you for trusting Austin Mobile Marine Mechanic to service your vessel.  Please allow up to three days for scheduling. If you do not receive an option for scheduling within that timeframe, please feel free to call or send a text message to 512 200 3433.\n\nSincerely,\n\nJason Sinn\nAustin Mobile Marine Mechanic\n512 200 3433`,
             }
-          }).then(() => console.log('Queued email for delivery!'));
+          }).then(() => console.log('Queued email for delivery!'));*/
       });
+      console.log('storage', localStorage.getItem('ownerRefId'));
     };
